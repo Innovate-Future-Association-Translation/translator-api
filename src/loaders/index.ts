f77ec413d713b7db9844955b3db02ac78a7e03d0
Steven Ren <stevenfrog@outlook.com>
2025-03-22 16:51:13 +1030

    feat: Migrate to MongoDB Atlas -update Readme File related to mongoDb Atlas connection -remove console log(db connection url in nongoose.ts) -create Atlas db and invited team members(dev and devOps) to Atlas Project -Test the Connection of the URL via Mongodb Compass and the other laptop
    update:
    -remove url from readme and replace it by hint
    
    resolve IT-30
    
    feat/IT-30-Migrate-To-MongoDB-Atlas
    
    -Remove sapcing in server start up message
    -Add database connection message once server connected to atlas cloud platform
    
    Debug IT-30

--- src/loaders/index.ts
+++ src/loaders/index.ts
@@ -6,7 +6,7 @@ const init = async (): Promise<void> => {
   try {
     await connectDB();
     startServer();
-    console.log(" Server and database initialized successfully.");
+    console.log("Server and database initialized successfully.");
   } catch (error) {
     console.error(AppErrorMessages.APP_FAIL_INITIALIZATION, error);
     process.exit(1);
