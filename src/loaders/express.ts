13890e9ae888c30dfacb43e1958000fae2a2eda7
Steven Ren <stevenfrog@outlook.com>
2025-03-29 22:46:21 +1030

    feat/18-Third-party-sign-up-and-login -Add google oauth auth for backend -Update user model with google id and set password of user model is not required if and only if the user are google user -add fetch profile route and controller for getUserProfile -implement auth middle ware for jwt token generated after google oauth for get user profile -set up redirect callback link and javascript link used for google oauth
    resolve IT-18
    
    feat/18-Third-party-sign-up-and-login
    -fix redirct issue when login fail
    
    bugfix it-18
    
    feat/18-Third-party-sign-up-and-login
    
    update: change the redirection of OAuth failure path to signUp page with error params
    
    Resolve IT-18
    
    Update
    -Change the login redirected URL from signup page to login-callback
    
    Update
    -remove passport.ts from config to middleware
    -reset token expiry time to 15 mins
    
    Bugfix
    -remove  console log which exposes the user profile from google
    
    Bugfix IT-18
    
    Update
    -rename config.ts to index.ts
    -remove magic string in user.controller.ts line 118
    -remove commented code in user model
    -remove double empty line in express.ts
    
    debug it -18

--- src/loaders/express.ts
+++ src/loaders/express.ts
@@ -9,34 +9,60 @@ import helmet from "helmet";
 import cors from "cors";
 import errorHandler from "../middlewares/ErrorHandler";
 import { authErrorMessages } from "../utils/errorMessages";
+import passport from "../middlewares/thirdPartyAuth/passport";
+import session from "express-session";
 
 const swaggerSpec = swaggerJSDoc(swaggerOptions);
 
 const startServer = () => {
   const app = express();
-  app.use(cors());
-  //we make morgan as a utils file later on and used it to log the request
+
+  app.use(cors({
+    origin: 'http://localhost:3000',
+    credentials: true, 
+  }));
+
   app.use(morgan("combined"));
-  app.use(helmet());
+  app.use(helmet());  
   app.use(express.json());
+
+  app.use(
+    session({
+      secret: config.sessionSecret,
+      resave: true,
+      saveUninitialized: true,
+      cookie: {
+        secure: false, 
+        httpOnly: true,
+        maxAge: 24 * 60 * 60 * 1000, 
+      },
+    })
+  );
+  app.use(passport.initialize());
+  app.use(passport.session());
+
   app.use(config.api.prefix, router);
+
   app.use(
     config.swaggerDocsPath,
     swaggerUi.serve,
     swaggerUi.setup(swaggerSpec)
   );
 
+ 
   app.use(errorHandler);
 
-  // Basic JWT config check
+ 
   if (!config.jwtSecret) {
     console.warn(authErrorMessages.JWT_CONFIG_ERROR);
   }
 
+  // Start the server
   const server = app.listen(config.port, () => {
     console.log("SERVER STARTED:", config.port);
   });
 
+  // Handle server errors
   server.on("error", (err: Error) => {
     console.error("Server error:", err.message);
     process.exit(1);
